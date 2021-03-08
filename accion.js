6fe3373bf458618402ae11e2e0ad9588887b6949
kutedo2001 <73166242+kutedo2001@users.noreply.github.com>
2021-03-08 11:34:06 +0100

    Add files via upload

---
+++ accion.js
@@ -0,0 +1,14 @@
+
+function mostrar() {
+    document.getElementById("menuabierto").style.width = "300px";
+    document.getElementById("menuabierto").style.marginLeft = "300px";
+    document.getElementById("abrir").style.display = "none";
+    document.getElementById("cerrar").style.display = "inline";
+}
+
+function ocultar() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("contenido").style.marginLeft = "0";
+    document.getElementById("abrir").style.display = "inline";
+    document.getElementById("cerrar").style.display = "none";
+}
