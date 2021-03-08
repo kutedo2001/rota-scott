5e61df0768b7713adbafe8336aa428875778707b
kutedo2001 <1secuncala@gmail.com>
2021-03-08 12:39:36 +0100

    .

--- accion.js
+++ accion.js
@@ -1,7 +1,6 @@
-
 function mostrar() {
     document.getElementById("menuabierto").style.width = "300px";
-    document.getElementById("menuabierto").style.marginLeft = "300px";
+    document.getElementById("menuabierto").style.marginLeft = "0";
     document.getElementById("abrir").style.display = "none";
     document.getElementById("cerrar").style.display = "inline";
 }
