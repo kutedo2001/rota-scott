6317fd0ae54a2e2703fa89dd62817399b713256c
kutedo2001 <1secuncala@gmail.com>
2021-03-09 08:23:06 +0100

    .

--- transiciones.js
+++ transiciones.js
@@ -1,12 +1,13 @@
-function mostrar() {
-    document.getElementById("menuabierto").style.width = "300px";
-    document.getElementById("menuabierto").style.marginRight = "500px";
+function abrir() {
+    document.getElementById("textomenu").style.width = "300px";
+    document.getElementById("textomenu").style.marginRight = "500px";
     document.getElementById("abrirmenu").style.display = "none";
-    document.getElementById("menucerrar").style.display = "inline";
+    document.getElementById("cerrarmenu").style.display = "inline";
 }
+
 function cerrar() {
-    document.getElementById("menucerrar").style.width = "300px";
-    document.getElementById("menucerrar").style.marginRight = "1000px";
+    document.getElementById("cerrarmenu").style.width = "300px";
+    document.getElementById("menucerrar").style.marginRight = "1000000px";
     document.getElementById("abrirmenu").style.display = "inline";
     document.getElementById("cerrarmenu").style.display = "none";
 }
